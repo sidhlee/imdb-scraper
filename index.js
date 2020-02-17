cbb95f11e0fbd99ab08b1938f29d29a36133e5fb
Hayoun Lee <sidhlee@gmail.com>
2020-02-17 18:02:49 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// define what data we want
+const sampleResult = {
+  title: "Parasite",
+  rank: 1,
+  imdbRating: 8.6,
+  descriptionUrl:
+    "https://www.imdb.com/title/tt6751668/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=ea4e08e1-c8a3-47b5-ac3a-75026647c16e&pf_rd_r=3EJ81GA2FCT7ZNMXG620&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=moviemeter&ref_=chtmvm_tt_1",
+  posterUrl:
+    "https://www.imdb.com/title/tt6751668/mediaviewer/rm3194916865"
+};
